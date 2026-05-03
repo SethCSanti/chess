38535c445f7a15de20686fa8f1d765b9fcdfb8c7
SethCSanti <sethcconner@gmail.com>
2026-05-02 20:24:23 -0600

    0: overriding for equals() and hashCode()

--- shared/src/main/java/chess/SlidingMoves.java
+++ shared/src/main/java/chess/SlidingMoves.java
@@ -1,5 +1,8 @@
 package chess;
 
+/**
+ * This class is mainly for the Queen, Rook, and Bishop.
+ */
 public class SlidingMoves {
     public void calculateDiagonals(ChessBoard board, ChessPosition myPosition) {}
 
