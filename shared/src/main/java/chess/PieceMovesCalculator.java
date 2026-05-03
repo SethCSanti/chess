4943ea52b455436e3a7173e9a76ddaab7d1a40b5
SethCSanti <sethcconner@gmail.com>
2026-05-02 20:03:19 -0600

    0: abstracted piece moves into two different classes

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,4 +1,43 @@
 package chess;
 
+import java.util.Collection;
+import java.util.List;
+
 public class PieceMovesCalculator {
+
+    public static Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+
+        return List.of();
+    }
+
+    public static Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+
+        return List.of();
+    }
+
+    public static Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+
+        return List.of();
+    }
+
+    public static Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+
+        return List.of();
+    }
+
+    public static Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+
+        return List.of();
+    }
+
+    public static Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+
+        return List.of();
+    }
 }
---
+++ shared/src/main/java/chess/SlidingMoves.java
@@ -0,0 +1,7 @@
+package chess;
+
+public class SlidingMoves {
+    public void calculateDiagonals(ChessBoard board, ChessPosition myPosition) {}
+
+    public void calculateSides(ChessBoard board, ChessPosition myPosition) {}
+}
