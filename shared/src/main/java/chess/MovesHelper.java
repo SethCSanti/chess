5e42add9536a94850ffc8fc7ab145166505162f4
SethCSanti <sethcconner@gmail.com>
2026-05-04 20:00:52 -0600

    0: added method for diagonal slides, refactored duplicate code for slides into abstracted method

--- shared/src/main/java/chess/MovesHelper.java
+++ shared/src/main/java/chess/MovesHelper.java
@@ -8,16 +8,9 @@ import java.util.Collection;
  * but also the checks for King and Knight.
  */
 public class MovesHelper {
-    public void calculateDiagonals(ChessBoard board, ChessPosition myPosition) {}
-
-    public Collection<ChessMove> calculateSides(ChessBoard board, ChessPosition myPosition) {
+    public static Collection<ChessMove> sliderHelper(ChessBoard board, ChessPosition myPosition, int[][] directions) {
         Collection<ChessMove> moves = new ArrayList<>();
         ChessPiece piece = board.getPiece(myPosition);
-
-        int[][] directions = {
-                {1, 0}, {-1, 0}, {0, 1}, {0, -1}
-        };
-
         for (int[] direction : directions) {
             int step = 1;
             int newRow = myPosition.getRow() + (direction[0] * step);
@@ -40,6 +33,22 @@ public class MovesHelper {
             }
         }
         return moves;
+    }
+
+    public static Collection<ChessMove> calculateDiagonals(ChessBoard board, ChessPosition myPosition) {
+        int[][] directions = {
+                {1, 1}, {-1, 1}, {-1, -1}, {1, -1}
+        };
+
+        return sliderHelper(board, myPosition, directions);
+    }
+
+    public static Collection<ChessMove> calculateSides(ChessBoard board, ChessPosition myPosition) {
+        int[][] directions = {
+                {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };
+
+        return sliderHelper(board, myPosition, directions);
     }
 
     public static Collection<ChessMove> jumpMoves(ChessBoard board, ChessPosition myPosition, int[][] offsets) {
