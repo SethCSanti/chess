f0fe45ae9644ab940e5a10a69fb2469fe33bfab5
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:44:53 -0600

    3: implemented the modularized handler for creategame

--- server/src/main/java/server/CreateGameHandler.java
+++ server/src/main/java/server/CreateGameHandler.java
@@ -1,4 +1,23 @@
 package server;
 
+import dataaccess.DataAccess;
+import io.javalin.http.Context;
+import request.CreateGameRequest;
+import service.GameService;
+
 public class CreateGameHandler {
+
+    private final GameService gameService;
+
+    public CreateGameHandler(DataAccess dataAccess) {
+        this.gameService = new GameService(dataAccess);
+    }
+
+    public void handle(Context ctx) throws Exception {
+        String authToken = ctx.header("authorization");
+        CreateGameRequest request = JsonUtils.fromJson(ctx.body(), CreateGameRequest.class);
+        var result = gameService.createGame(authToken, request);
+        ctx.status(200);
+        ctx.result(JsonUtils.toJson(result));
+    }
 }
