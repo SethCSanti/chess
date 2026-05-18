3380c8645c7442f505255b5098e8e2f05992ffe2
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:24:46 -0600

    3: implemented the modularized handler for clear

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -1,4 +1,21 @@
 package server;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import service.ClearService;
+
 public class ClearHandler {
+
+    private final ClearService clearService;
+
+    public ClearHandler(DataAccess dataAccess) {
+        this.clearService = new ClearService(dataAccess);
+    }
+
+    public void handle(Context ctx) throws DataAccessException {
+        clearService.clear();
+        ctx.status(200);
+        ctx.result(JsonUtils.toJson(new Response()));
+    }
 }
