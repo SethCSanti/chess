d102c974663b91fbe804a926b8d03520eccb8989
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:07:41 -0600

    3: created files for endpoint handlers and then implemented the handlers as fields and routes in the main server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import dataaccess.MemoryDataAccess;
 import io.javalin.*;
 
 public class Server {
@@ -8,8 +9,25 @@ public class Server {
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        MemoryDataAccess dataAccess = new MemoryDataAccess();
 
-        // Register your endpoints and exception handlers here.
+        // store handlers as fields
+        ClearHandler clearHandler = new ClearHandler(dataAccess);
+        RegisterHandler registerHandler = new RegisterHandler(dataAccess);
+        LoginHandler loginHandler = new LoginHandler(dataAccess);
+        LogoutHandler logoutHandler = new LogoutHandler(dataAccess);
+        ListGamesHandler listGamesHandler = new ListGamesHandler(dataAccess);
+        CreateGameHandler createGameHandler = new CreateGameHandler(dataAccess);
+        JoinGameHandler joinGameHandler = new JoinGameHandler(dataAccess);
+
+        // register routes
+        javalin.delete("/db", clearHandler::handle);
+        javalin.post("/user", registerHandler::handle);
+        javalin.post("/session", loginHandler::handle);
+        javalin.delete("/session", logoutHandler::handle);
+        javalin.get("/game", listGamesHandler::handle);
+        javalin.post("/game", createGameHandler::handle);
+        javalin.put("/game", joinGameHandler::handle);
 
     }
 
