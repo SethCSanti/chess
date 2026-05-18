e4daa88ab0d4cde7583c117cd6d1da71811b862b
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:32:01 -0600

    3: implemented the modularized handler for login

--- server/src/main/java/server/LoginHandler.java
+++ server/src/main/java/server/LoginHandler.java
@@ -1,4 +1,22 @@
 package server;
 
+import dataaccess.DataAccess;
+import io.javalin.http.Context;
+import request.LoginRequest;
+import service.UserService;
+
 public class LoginHandler {
+
+    private final UserService userService;
+
+    public LoginHandler(DataAccess dataAccess) {
+        this.userService = new UserService(dataAccess);
+    }
+
+    public void handle(Context ctx) throws Exception {
+        LoginRequest request = JsonUtils.fromJson(ctx.body(), LoginRequest.class);
+        var result = userService.loginUser(request);
+        ctx.status(200);
+        ctx.result(JsonUtils.toJson(result));
+    }
 }
