275273c79110e1bb51276a12554b15d7c9beb1ab
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:47:52 -0600

    3: implemented the modularized handler for joingame

--- server/src/main/java/server/JoinGameHandler.java
+++ server/src/main/java/server/JoinGameHandler.java
@@ -1,4 +1,23 @@
 package server;
 
+import dataaccess.DataAccess;
+import io.javalin.http.Context;
+import request.JoinGameRequest;
+import service.GameService;
+
 public class JoinGameHandler {
+
+    private final GameService gameService;
+
+    public JoinGameHandler(DataAccess dataAccess) {
+        this.gameService = new GameService(dataAccess);
+    }
+
+    public void handle(Context ctx) throws Exception {
+        String authToken = ctx.header("authorization");
+        JoinGameRequest request = JsonUtils.fromJson(ctx.body(), JoinGameRequest.class);
+        gameService.joinGame(authToken, request);
+        ctx.status(200);
+        ctx.result(JsonUtils.toJson(new Response()));
+    }
 }
