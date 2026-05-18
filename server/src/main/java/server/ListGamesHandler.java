51c732193e2ee6b80659b1a7f3966e9e6369c809
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:40:26 -0600

    3: implemented the modularized handler for listgames

--- server/src/main/java/server/ListGamesHandler.java
+++ server/src/main/java/server/ListGamesHandler.java
@@ -1,4 +1,21 @@
 package server;
 
+import dataaccess.DataAccess;
+import io.javalin.http.Context;
+import service.GameService;
+
 public class ListGamesHandler {
+
+    private final GameService gameService;
+
+    public ListGamesHandler(DataAccess dataAccess) {
+        this.gameService = new GameService(dataAccess);
+    }
+
+    public void handle(Context ctx) throws Exception {
+        String authToken = ctx.header("authorization");
+        var result = gameService.listGames(authToken);
+        ctx.status(200);
+        ctx.result(JsonUtils.toJson(result));
+    }
 }
