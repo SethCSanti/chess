476e2eb0cc321569cb9e8e1a8292530bcf9ba7d0
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:36:56 -0600

    3: implemented the modularized handler for logout

--- server/src/main/java/server/LogoutHandler.java
+++ server/src/main/java/server/LogoutHandler.java
@@ -1,4 +1,22 @@
 package server;
 
+import dataaccess.DataAccess;
+import io.javalin.http.Context;
+import request.LogoutRequest;
+import service.UserService;
+
 public class LogoutHandler {
+
+    private final UserService userService;
+
+    public LogoutHandler(DataAccess dataAccess) {
+        this.userService = new UserService(dataAccess);
+    }
+
+    public void handle(Context ctx) throws Exception {
+        String authToken = ctx.header("authorization");
+        userService.logoutUser(new LogoutRequest(authToken));
+        ctx.status(200);
+        ctx.result(JsonUtils.toJson(new Response()));
+    }
 }
