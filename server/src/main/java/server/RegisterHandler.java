31b5d50cdc66d8cfab137ea9da27c3061459de50
SethCSanti <sethcconner@gmail.com>
2026-05-18 16:28:18 -0600

    3: implemented the modularized handler for register

--- server/src/main/java/server/RegisterHandler.java
+++ server/src/main/java/server/RegisterHandler.java
@@ -1,4 +1,22 @@
 package server;
 
+import dataaccess.DataAccess;
+import io.javalin.http.Context;
+import request.RegisterRequest;
+import service.UserService;
+
 public class RegisterHandler {
+
+    private final UserService userService;
+
+    public RegisterHandler(DataAccess dataAccess) {
+        this.userService = new UserService(dataAccess);
+    }
+
+    public void handle(Context ctx) throws Exception {
+        RegisterRequest request = JsonUtils.fromJson(ctx.body(), RegisterRequest.class);
+        var result = userService.registerUser(request);
+        ctx.status(200);
+        ctx.result(JsonUtils.toJson(result));
+    }
 }
