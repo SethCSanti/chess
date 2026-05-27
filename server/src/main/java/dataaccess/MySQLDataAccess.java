83c6fef6857bf0f3ffd7564cb764032db4fe25fa
SethCSanti <sethcconner@gmail.com>
2026-05-26 21:04:11 -0600

    4: implemented the updateGame method in MySQLDataAccess

--- server/src/main/java/dataaccess/MySQLDataAccess.java
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -174,7 +174,30 @@ public class MySQLDataAccess implements DataAccess {
     }
 
     @Override
-    public void updateGame(GameData game) throws DataAccessException { }
+    public void updateGame(GameData game) throws DataAccessException {
+        var statement = """
+        UPDATE games 
+        SET whiteUsername=?, blackUsername=?, gameName=?, game=? 
+        WHERE gameID=?
+        """;
+
+        String gameJson = JsonUtils.toJson(game.game());
+
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.setString(1, game.whiteUsername());
+            ps.setString(2, game.blackUsername());
+            ps.setString(3, game.gameName());
+            ps.setString(4, gameJson);
+            ps.setInt(5, game.gameID());
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to update game", e);
+        }
+    }
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
