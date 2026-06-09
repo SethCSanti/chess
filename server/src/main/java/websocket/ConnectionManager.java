6bd88c29e046ae0771dd21014fb85f75ffa2b3a7
SethCSanti <sethcconner@gmail.com>
2026-06-08 19:31:03 -0600

    6: implemented ConnectionManager based on the petshop example code

---
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -0,0 +1,42 @@
+package websocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    private final ConcurrentHashMap<Integer, Set<Session>> connections = new ConcurrentHashMap<>();
+
+    public void add(int gameID, Session session) {
+        connections.computeIfAbsent(gameID, k -> ConcurrentHashMap.newKeySet()).add(session);
+    }
+
+    public void remove(Session session) {
+        for (Set<Session> sessions : connections.values()) {
+            sessions.remove(session);
+        }
+    }
+
+    public void broadcast(int gameID, Session excludeSession, ServerMessage message) throws IOException {
+        broadcastHelper(gameID, excludeSession, message);
+    }
+
+    public void broadcastToAll(int gameID, ServerMessage message) throws IOException {
+        broadcastHelper(gameID, null, message);
+    }
+
+    private void broadcastHelper(int gameID, Session excludeSession, ServerMessage message) throws IOException {
+        Set<Session> sessions = connections.get(gameID);
+        if (sessions == null) { return; }
+        String msg = new Gson().toJson(message);
+        for (Session session : sessions) {
+            if (session.isOpen() && !session.equals(excludeSession)) {
+                session.getRemote().sendString(msg);
+            }
+        }
+    }
+}
